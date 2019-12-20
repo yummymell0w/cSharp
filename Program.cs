c4ad0ad5ed056f20b6c1dede72ac2a28f5ccff18
Margarita Artiushenko <margoart.work@gmail.com>
2019-12-19 23:09:50 -0800

    added new problem

--- Program.cs
+++ Program.cs
@@ -6,52 +6,57 @@ namespace Problems
     {
         static public void Main(string[] args)
         {
-            string ipAddress = "255.100.50.0";
-            DefangingIpAddress object1 = new DefangingIpAddress();
-            Console.WriteLine(object1.DefangIPaddr(ipAddress));
+            int n = 234;
+            SubtractProductAndSum newObject = new SubtractProductAndSum();
+            Console.WriteLine(newObject.SubtractProduct(n));
+            /*
+                        string ipAddress = "255.100.50.0";
+                        DefangingIpAddress object1 = new DefangingIpAddress();
+                        Console.WriteLine(object1.DefangIPaddr(ipAddress));
 
-            string J = "aA";
-            string S = "aAAbbbb";
-            JewelsAndStones object2 = new JewelsAndStones();
-            Console.WriteLine(object2.NumJewelsInStones(S, J));
+                        string J = "aA";
+                        string S = "aAAbbbb";
+                        JewelsAndStones object2 = new JewelsAndStones();
+                        Console.WriteLine(object2.NumJewelsInStones(S, J));
 
-            int[] A = { 1, 2, 3, 3 };
-            NRepeatedElement object3 = new NRepeatedElement();
-            Console.WriteLine(object3.RepeatedNTimes(A));
+                        int[] A = { 1, 2, 3, 3 };
+                        NRepeatedElement object3 = new NRepeatedElement();
+                        Console.WriteLine(object3.RepeatedNTimes(A));
 
-            int left = 1;
-            int right = 2;
-            SelfDividingNumbers object4 = new SelfDividingNumbers();
-            Console.WriteLine(object4.SelfDividingNumber(left, right));
+                        int left = 1;
+                        int right = 2;
+                        SelfDividingNumbers object4 = new SelfDividingNumbers();
+                        Console.WriteLine(object4.SelfDividingNumber(left, right));
 
-            int[] B = { 3, 1, 2, 4 };
-            SortArrayByParity object6 = new SortArrayByParity();
-            Console.WriteLine(object6.SortArray(B));
+                        int[] B = { 3, 1, 2, 4 };
+                        SortArrayByParity object6 = new SortArrayByParity();
+                        Console.WriteLine(object6.SortArray(B));
 
-            int[] C = { 3, 1, 2, 4 };
-            SortArrayByParityViaList object7 = new SortArrayByParityViaList();
-            Console.WriteLine(object7.SortArrayByParityList(C));
+                        int[] C = { 3, 1, 2, 4 };
+                        SortArrayByParityViaList object7 = new SortArrayByParityViaList();
+                        Console.WriteLine(object7.SortArrayByParityList(C));
 
-            string D = "RLRRLLRLRL";
-            SplitBalancedString object8 = new SplitBalancedString();
-            Console.WriteLine(object8.BalancedStringSplit(D));
+                        string D = "RLRRLLRLRL";
+                        SplitBalancedString object8 = new SplitBalancedString();
+                        Console.WriteLine(object8.BalancedStringSplit(D));
 
-            int[] E = { -4, -1, 0, 3, 10 };
-            SquaresOfSortedArray object9 = new SquaresOfSortedArray();
-            Console.WriteLine(object9.SortedSquares(E));
+                        int[] E = { -4, -1, 0, 3, 10 };
+                        SquaresOfSortedArray object9 = new SquaresOfSortedArray();
+                        Console.WriteLine(object9.SortedSquares(E));
 
-            string str = "Hello";
-            ToLowerCase object10 = new ToLowerCase();
-            Console.WriteLine(object10.ConvertToLowerCase(str));
+                        string str = "Hello";
+                        ToLowerCase object10 = new ToLowerCase();
+                        Console.WriteLine(object10.ConvertToLowerCase(str));
 
-            string[] words = { "gin", "zen", "gig", "msg" };
-            UniqueMorseCodeWords object11 = new UniqueMorseCodeWords();
-            Console.WriteLine(object11.ConvertAllWordsToMorseCode(words));
+                        string[] words = { "gin", "zen", "gig", "msg" };
+                        UniqueMorseCodeWords object11 = new UniqueMorseCodeWords();
+                        Console.WriteLine(object11.ConvertAllWordsToMorseCode(words));
 
-            int[] arr = { 1, 2, 2, 1, 1, 3 };
-            UniqueNumberOccurrences object12 = new UniqueNumberOccurrences();
-            Console.WriteLine(object12.UniqueOccurrences(arr));
-            
+                        int[] arr = { 1, 2, 2, 1, 1, 3 };
+                        UniqueNumberOccurrences object12 = new UniqueNumberOccurrences();
+                        Console.WriteLine(object12.UniqueOccurrences(arr));
+                        */
+
         }
     }
 }
---
+++ SubtractProductAndSum.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class SubtractProductAndSum
+{
+    public int SubtractProduct(int n)
+    {
+        List<int> listOfNumbers = new List<int>();
+        for (; n>0; n/=10)
+        {
+        listOfNumbers.Add(n % 10);
+        }
+
+        int[] numbers = listOfNumbers.ToArray();
+
+        int product = 1;
+        int sum = 0;
+        // Product of digits = 2 * 3 * 4 = 24 
+        foreach(int x in numbers)
+        {
+            product *= x;
+        }
+        //Sum of digits = 2 + 3 + 4 = 9 
+        foreach (int y in numbers)
+        {
+            sum += y;
+        }
+        //Result = 24 - 9 = 15
+        int result = product - sum;
+        return result;
+
+    }
+}
