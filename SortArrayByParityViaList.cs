0d09e5ab5f5845c9035aed80b8450a23e7e57968
Margarita Artiushenko <margoart.work@gmail.com>
2020-01-02 23:20:03 -0800

    Minor updates

--- SortArrayByParityViaList.cs
+++ SortArrayByParityViaList.cs
@@ -8,20 +8,16 @@ namespace Problems
         {
             List<int> B = new List<int>();
             List<int> C = new List<int>();
-            int j = 0;
-            int k = 0;
-
+            
             for (int i = 0; i < A.Length; i++)
             {
                 if (A[i] % 2 == 0)
                 {
                     B.Add(A[i]);
-                    j++;
                 }
                 else
                 {
                     C.Add(A[i]);
-                    k++;
                 }
             }
 
