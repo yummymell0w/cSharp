94fffe3dad2cb53b560486ee8a72a48ae5d399ff
Margarita Artiushenko <margoart.work@gmail.com>
2020-01-09 23:15:38 -0800

    Update ReplaceWithGreatestElement.cs

--- ReplaceWithGreatestElement.cs
+++ ReplaceWithGreatestElement.cs
@@ -7,18 +7,12 @@ public class ReplaceWithGreatestElement
 
         int[] sortedArr = new int[arr.Length];
         int max = 1;
-        for (int i = arr.Length-2; i >=0; i--)
+        for (int i = arr.Length - 2; i >= 0; i--)
         {
-            //for (int j = i + 1; j < arr.Length; j++)
-            //{
-
-                if (max < arr[i+1])
-                {
-                    max = arr[i+1];
-                }
-               
-            //}
-
+            if (max < arr[i + 1])
+            {
+                max = arr[i + 1];
+            }
             sortedArr[i] = max;
         }
         sortedArr[arr.Length - 1] = -1;
