0c3702347a53140f82143b7c1964c72d63148546
Margarita Artiushenko <margoart.work@gmail.com>
2020-01-24 23:04:52 -0800

    updated algorithm

--- IntersectionOfArraysII.cs
+++ IntersectionOfArraysII.cs
@@ -11,52 +11,29 @@ namespace Problems
     {
         public static int[] Intersect(int[] nums1, int[] nums2)
         {
-            //Input: nums1 = [1,2,2,1], nums2 = [2,2]
-            //Output: [2,2]
-            Dictionary<int, int> dict1 = new Dictionary<int, int>();
-
+            Dictionary<int, int> dick = new Dictionary<int, int>();
             for (int i = 0; i < nums1.Length; i++)
             {
-                if (dict1.TryGetValue(nums1[i], out int count))
+                if (dick.TryGetValue(nums1[i], out int count))
                 {
-                    dict1[nums1[i]] = count + 1;
+                    dick[nums1[i]] = count + 1;
                 }
                 else
                 {
-                    dict1.Add(nums1[i], 1);
-
+                    dick[nums1[i]] = 1;
                 }
             }
 
-            Dictionary<int, int> dict2 = new Dictionary<int, int>();
-
+            List<int> result = new List<int>(nums1.Length);
             for (int j = 0; j < nums2.Length; j++)
             {
-                if (dict2.TryGetValue(nums2[j], out int count))
+                if (dick.TryGetValue(nums2[j], out int count))
                 {
-                    dict2[nums2[j]] = count + 1;
-                }
-                else
-                {
-                    dict2.Add(nums2[j], 1);
-
-                }
-            }
-
-            List<int> result = new List<int>();
-
-            foreach (var number in dict1)
-            {
-                int min = number.Value;
-                dict2.TryGetValue(number.Key, out int count);
-                if (min > count)
-                {
-                    min = count;
-                }
-
-                for (; min > 0; min--)
-                {
-                    result.Add(number.Key);
+                    if (count > 0)
+                    {
+                        result.Add(nums2[j]);
+                        dick[nums2[j]] = count - 1;
+                    }
                 }
             }
 
