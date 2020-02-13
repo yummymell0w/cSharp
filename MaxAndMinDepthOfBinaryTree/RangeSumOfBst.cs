f2a905ed7fa0aa311dd3c351cf9ed6d02d17edee
Margarita Artiushenko <margoart.work@gmail.com>
2020-02-12 22:57:27 -0800

    new leetcode problem.

---
+++ MaxAndMinDepthOfBinaryTree/RangeSumOfBst.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problems.MaxAndMinDepthOfBinaryTree
+{
+    class RangeSumOfBst
+    {
+        public int RangeSumBST(TreeNode root, int L, int R)
+        {
+            //Input: root = [10, 5, 15, 3, 7, null, 18],
+            //L = 7,
+            //R = 15
+
+            if (root == null)
+            {
+                return 0;
+            }
+
+            var sum = 0;
+            if (L <= root.val && root.val <= R)
+            {
+                sum = root.val;
+            }
+
+            var left = RangeSumBST(root.left, L, R);
+            var right = RangeSumBST(root.right, L, R);
+
+            return left + right + sum;
+        }
+    }
+}
